a19e1d4fcdc7d0a8b5a37e9bdceb42fd58a52df2
eons <eons@eons.dev>
2022-09-26 19:45:08 -0700

    test and debug

--- inc/api_delete.py
+++ inc/api_delete.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import apie
+from api_external import external
 
 class delete(external):
     def __init__(this, name="delete"):
@@ -10,15 +11,13 @@ class delete(external):
 
         #this.allowedNext = ['help'] only
 
-        this.helpText = '''\
+        this.helpText = f'''\
 Delete any resource by offloading the actual work to another API.
 This does not (currently) have access to the local filesystem (e.g. in case you wanted to delete an uploaded file).
 
 Per the parent 'external':
+{this.helpText}
 '''
-        this.helpText += super().helpText
-
-
 
     def Call(this):
         # This is a very simple wrapper at the moment.
